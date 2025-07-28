6dc17d6ff9e64636dc2f46167cf9878dc4ec56fb
Dharmik Panchal <dharmikp572+github@gmail.com>
2025-07-28 17:35:57 +0000

    deploy

--- .github/generate-static.sh
+++ .github/generate-static.sh
@@ -4,9 +4,9 @@ git checkout site
 git rebase origin/main
 pnpm run build
 mkdir tmp
-mv ./ tmp/
+mv ./ ./tmp/
 mv ./tmp/build ./
 rm -rf ./tmp
-ga .
-gc -m 'deploy'
-git push --force-with-lease
+git add .
+git commit -m 'deploy'
+# git push --force-with-lease
